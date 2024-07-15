2796453a5bf32c62de9e270d224af6acee343761
Shalu <shalu.priya@siemens.com>
2024-07-15 22:38:52 +0530

    Added DbContext File

--- DbOperationsWithEfCoreApp/DbOperationsWithEfCoreApp/Program.cs
+++ DbOperationsWithEfCoreApp/DbOperationsWithEfCoreApp/Program.cs
@@ -1,4 +1,7 @@
 
+using DbOperationsWithEfCoreApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace DbOperationsWithEfCoreApp
 {
     public class Program
@@ -6,6 +9,8 @@ namespace DbOperationsWithEfCoreApp
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AppDb")));
 
             // Add services to the container.
 
